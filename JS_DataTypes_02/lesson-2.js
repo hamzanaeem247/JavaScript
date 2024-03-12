81dc114429df06d6184ec93951a6c2341552d2f4
Hamza Naeem <85546609+hamzanaeem247@users.noreply.github.com>
2024-03-12 08:43:00 +0000

    done

--- JS_DataTypes_02/lesson-2.js
+++ JS_DataTypes_02/lesson-2.js
@@ -2,3 +2,22 @@
 // alert(3+3) // we are using node.js, not browser.
 console.log(3 + 3);
 console.log("Hamza");
+
+// number
+// bigInt
+// String
+// Boolean
+// null
+// Undefined
+// Symbol
+
+// Object
+
+let name = "Hamza";
+let age = 18;
+let isLoggedIn = false;
+
+console.log(name + " " + age + " " + isLoggedIn);
+console.log(typeof name)
+console.log(typeof null)
+console.log(typeof undefined)
