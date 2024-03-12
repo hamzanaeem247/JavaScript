a5dac07fd7495779e2875bd22ccfdee1e293bbf8
Hamza Naeem <85546609+hamzanaeem247@users.noreply.github.com>
2024-03-12 07:50:27 +0000

    save

---
+++ JS_Basics_01/lesson-1.js
@@ -0,0 +1,15 @@
+const accountId = 144;
+let accountEmail = "hamzanaeem@gmail.com";
+var accountPassword = "12345";
+accountCity = "Lahore";
+// accountId = 6; // Not Allowed
+accountEmail = "hamza@gmail.com";
+accountPassword = "123";
+accountCity = "Karachi";
+
+console.log(accountId)
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountCity)
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
